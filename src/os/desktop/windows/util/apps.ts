2811e18efb8ac998ac9b629e9087a7df2050b623
Christopher Seven Phiri <chris7phiri@gmail.com>
2023-09-14 20:20:38 +0200

    update

--- src/os/desktop/windows/util/apps.ts
+++ src/os/desktop/windows/util/apps.ts
@@ -1,48 +1,49 @@
-'use client';
+"use client";
 
 import { WinApp, allApps } from "../config/apps";
 
+const getStorage = (key: string) => {
+  // TODO: Fix this right now this will get ignored by next.js 13
+  if (typeof window !== 'undefined') {
+    const data = localStorage.getItem(key);
+    return data ? JSON.parse(data) : null;
+  }
+  return null;
+};
+
 export const getTaskbarApps = (): WinApp[] => {
-  const ids = localStorage.getItem("taskbar")
-    ? (JSON.parse(localStorage.getItem("taskbar") as "") as string[])
-    : [
-        "apps/system/settings",
-        "apps/system/taskmanager",
-        "apps/system/explorer",
-        "apps/user/chrome",
-      ];
+  const ids = (getStorage("taskbar") as string[]) ?? [
+    "apps/system/settings",
+    "apps/system/taskmanager",
+    "apps/system/explorer",
+    "apps/user/chrome",
+  ];
   return allApps.filter((app) => ids.includes(app.payload ?? ""));
 };
 
 export const getDesktopApps = (): WinApp[] => {
-  const ids = localStorage.getItem("desktop")
-    ? (JSON.parse(localStorage.getItem("desktop") as "") as string[])
-    : [
-        "apps/system/settings",
-        "apps/system/taskmanager",
-        "apps/system/explorer",
-        "apps/user/chrome",
-      ];
+  const ids = (getStorage("desktop") as string[]) ?? [
+    "apps/system/settings",
+    "apps/system/taskmanager",
+    "apps/system/explorer",
+    "apps/user/chrome",
+  ];
   return allApps.filter((app) => ids.includes(app.payload ?? ""));
 };
 
 export const getStartApps = () => {
-  const pinned = localStorage.getItem("pinned")
-    ? (JSON.parse(localStorage.getItem("pinned") as "") as string[])
-    : [
-        "apps/system/settings",
-        "apps/system/taskmanager",
-        "apps/system/explorer",
-        "apps/user/chrome",
-      ];
-  const recent = localStorage.getItem("recent")
-    ? (JSON.parse(localStorage.getItem("recent") as "") as string[])
-    : [
-        "apps/system/settings",
-        "apps/system/taskmanager",
-        "apps/system/explorer",
-        "apps/user/chrome",
-      ];
+  const pinned = (getStorage("pinned") as string[]) ?? [
+    "apps/system/settings",
+    "apps/system/taskmanager",
+    "apps/system/explorer",
+    "apps/user/chrome",
+  ];
+  const recent = (getStorage("recent") as string[]) ?? [
+    "apps/system/settings",
+    "apps/system/taskmanager",
+    "apps/system/explorer",
+    "apps/user/chrome",
+  ];
   return {
     pinned: allApps.filter((app) => pinned.includes(app.payload ?? "")),
     recent: allApps.filter((app) => recent.includes(app.payload ?? "")),
